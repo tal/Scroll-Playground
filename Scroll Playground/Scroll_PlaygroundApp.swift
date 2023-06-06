c7cd8115aea1d8ebfab2c9aad529e022bcac828f
Tal Atlas <me@tal.by>
2023-06-05 21:03:13 -0400

    Initial Commit

---
+++ Scroll Playground/Scroll_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scroll_PlaygroundApp.swift
+//  Scroll Playground
+//
+//  Created by Tal Atlas on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Scroll_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
